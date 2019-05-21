70ab4313159cf69a6ef429156e6699e7edf7ada8
Florian Heiwig <florian.hiwg@gmail.com>
2019-05-21 08:34:24 +0200

    second miniproject - email classification based on text and done with a svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -24,6 +26,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+clf = svm.SVC(gamma='scale')
+
+t0 = time()
+
+clf.fit(features_train, labels_train)
+
+print("training time:", round(time()-t0, 3), "s")
+
+pred = clf.predict(features_test)
+
+print(pred)
+
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
+
+print("Accuracy:", round(acc, 2))
+
 
 #########################################################
 
